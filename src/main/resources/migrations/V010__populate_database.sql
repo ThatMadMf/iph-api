e6ee71ba05aa20a559b4791fc2d31fa86819f587
Volodymyr Yesypenko <volodymyryesypenko@gmail.com>
2020-12-21 12:28:06 +0200

    Improve fixture

--- src/main/resources/migrations/V010__populate_database.sql
+++ src/main/resources/migrations/V010__populate_database.sql
@@ -18,26 +18,35 @@ INSERT INTO users(name, role)
 VALUES ('Alina', 'STUDENT'),
        ('Maxim', 'STUDENT'),
        ('Vova', 'STUDENT'),
-       ('name1', 'TEACHER'),
-       ('name2', 'TEACHER');
+       ('Oleg', 'STUDENT'),
+       ('Sasha', 'STUDENT'),
+       ('Sergiy Vassilyovich', 'TEACHER'),
+       ('Oleg Petrovich', 'TEACHER'),
+       ('Nadiya Vasylivna', 'TEACHER');
 
 INSERT INTO students(id, group_id)
 VALUES (1, 1),
        (2, 1),
-       (3, 2);
+       (3, 2),
+       (4, 2),
+       (5, 2);
 
 INSERT INTO teachers(id, degree)
-VALUES (4, '2 lvl'),
-       (5, '1 lvl');
+VALUES (6, '2 lvl'),
+       (7, '1 lvl'),
+       (8, '1 lvl');
 
 INSERT INTO subjects_teachers(subject_id, teacher_id)
-VALUES (1, 4),
-       (2, 4),
-       (3, 4),
-       (4, 5);
+VALUES (1, 6),
+       (2, 7),
+       (3, 7),
+       (4, 8);
 
 INSERT INTO publications(subject_id, title, text, author_id, creation_date, deadline)
-VALUES (1, 'Lab1', 'Need to write something', 4, now(), now() + interval '1' day),
-       (2, 'Announcement about deadline', '"Deadline delayed for 3 days', 5, now(), null),
-       (3, 'Lab2', 'Need to write something', 4, now(), null);
+VALUES (1, 'Lab1', 'Need to read documentation', 6, now(), now() + interval '1' day),
+       (2, 'Announcement about deadline', '"Deadline delayed for 3 days', 7, now(), null),
+       (3, 'Lab2', 'Need to complete a test', 6, now(), now() + interval '7' day),
+       (4, 'Announcement about schedule of exams', '"They are gonna be soon', 7, now(), null),
+       (5, 'Module test', '"For 3 previous lectures', 8, now(), now() + interval '14' day),
+       (6, 'Lab3', 'Write documentation', 6, now(), now() + interval '20' day);
 
